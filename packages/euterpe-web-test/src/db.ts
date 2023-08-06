9bf138561eb331016f6838d964263b3a01b6cb4e
Djkáťo <radovan.katrencik@gmail.com>
2023-08-06 23:05:38 +0200

    update euterpe-web-test imports

--- packages/euterpe-web-test/src/db.ts
+++ packages/euterpe-web-test/src/db.ts
@@ -1,60 +1,59 @@
-import { EuterpeBuilder, Library, Player } from "@euterpe.js/euterpe";
-//export const euterpe = new
-export const db = new Library.DB
+import { DB, Song, Artist, Ref, RefTo, Platforms } from "@euterpe.js/music-library"
+export const db = new DB
 
 db.add([
     //The IDs are added incrementally & are 0 based., so first artists ID added is 0, next 1 etc...
     //You can specify the ID manually if you want
-    new Library.Artist({
+    new Artist({
         name: "Jamie xx",
     }),
-    new Library.Artist({
+    new Artist({
         name: "janz",
     }),
-    new Library.Artist({
+    new Artist({
         name: "Machinedrum",
     }),
-    new Library.Artist({
+    new Artist({
         name: "Tanerélle",
     }),
-    new Library.Artist({
+    new Artist({
         name: "Mono/Poly",
     }),
-    new Library.Artist({
+    new Artist({
         name: "IMANU",
         links: [
-            [Library.Platforms.Spotify, new URL("https://open.spotify.com/artist/5Y7rFm0tiJTVDzGLMzz0W1?si=DRaZyugTTIqlBHDkMGKVqA&nd=1")]
+            [Platforms.Spotify, new URL("https://open.spotify.com/artist/5Y7rFm0tiJTVDzGLMzz0W1?si=DRaZyugTTIqlBHDkMGKVqA&nd=1")]
         ]
     }),
-    new Library.Artist({
+    new Artist({
         name: "toe",
         id: 10
     }),
 ])
 db.add([
-    new Library.Song({
+    new Song({
         //Refrences are constructed as such. This allows to get to the artist from either collection or song
-        artists: [new Library.Ref(Library.RefTo.Artists, 2), new Library.Ref(Library.RefTo.Artists, 3), new Library.Ref(Library.RefTo.Artists, 4)],
+        artists: [new Ref(RefTo.Artists, 2), new Ref(RefTo.Artists, 3), new Ref(RefTo.Artists, 4)],
         duration: 252,
         name: "Star",
-        remix_artists: [new Library.Ref(Library.RefTo.Artists, 5)],
+        remix_artists: [new Ref(RefTo.Artists, 5)],
         url: new URL("http://127.0.0.1:4200/Machinedrum, Tanerelle & Mono Poly - Star (IMANU Remix) final.mp3")
     }),
-    new Library.Song({
+    new Song({
         //If you don't like guessing the IDs, then this is also a way to do it
-        artists: [new Library.Ref(Library.RefTo.Artists, db.artists.find((a) => a.name == "Jamie xx")!.id!)],
+        artists: [new Ref(RefTo.Artists, db.artists.find((a) => a.name == "Jamie xx")!.id!)],
         duration: 331,
         name: "Sleep Sound",
         url: new URL("http://127.0.0.1:4200/Jamie xx - Sleep Sound.mp3")
     }),
-    new Library.Song({
-        artists: [new Library.Ref(Library.RefTo.Artists, 1)],
+    new Song({
+        artists: [new Ref(RefTo.Artists, 1)],
         duration: 75,
         name: "wish",
         url: new URL("http://127.0.0.1:4200/janz - wish.mp3")
     }),
-    new Library.Song({
-        artists: [new Library.Ref(Library.RefTo.Artists, 10)],
+    new Song({
+        artists: [new Ref(RefTo.Artists, 10)],
         duration: 4 * 60 + 5,
         name: "サニーボーイ・ラプソディ",
         url: new URL("http://127.0.0.1:4200/16.サニーボーイ・ラプソディ.ogg")
